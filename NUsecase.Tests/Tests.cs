f20119599e6d2475038ea1ac30b24bf48641e91a
Aleksandar Damjanovic <aleksandar.damjanovic@esentri.com>
2017-04-14 20:50:37 +0200

    added test project

---
+++ NUsecase.Tests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NUsecase.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
